4544c7846a81fbb0e40bea99aa6f7c8891b3a484
vaibhav <da-vaibhav@users.noreply.github.com>
2020-08-11 15:53:58 +0530

    added svg icons and other things in cards

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Card({ title, icon }) {
   return (
     <div className="ui-card">
-      <img src={icon} alt=""/>
+      <img src={icon} alt={title}/>
       <p>{title}</p>
     </div>
   )
--- src/Dashboard.js
+++ src/Dashboard.js
@@ -4,12 +4,14 @@ import Card from './Card';
 function Dashboard() {
   return (
     <div className="content-container">
-      <header>Dashboard</header>
-      <small className="sub-head">Mobile UX/UI design course</small>
+      <header>
+        <h2>Dashboard</h2>
+        <small className="sub-head">Mobile UX/UI design course</small>
+      </header>
       <div>
         <Card title="Students" icon="user.svg" />
-        <Card title="Average Mark"/>
-        <Card title="Underperforming students"/>
+        <Card title="Average Mark" icon="chart.svg"/>
+        <Card title="Underperforming students" icon="chart.svg"/>
       </div>
     </div>
   )
